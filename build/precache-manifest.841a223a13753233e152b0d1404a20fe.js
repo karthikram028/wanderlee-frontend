116e6fe3178b8dae89d6d3da3d73ff4bac7acbc3
Karthik Ram Chodapaneedi <karthik.r@Inventivess-MBP.attlocal.net>
2022-10-27 10:55:46 -0500

    adding files

---
+++ build/precache-manifest.841a223a13753233e152b0d1404a20fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf6a42d36b3569b590fbc10add5f72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0542e108eb7526e67a2",
+    "url": "/static/css/main.4c320705.chunk.css"
+  },
+  {
+    "revision": "ff4e965e7102dd2b3629",
+    "url": "/static/js/2.3e9e4915.chunk.js"
+  },
+  {
+    "revision": "c0542e108eb7526e67a2",
+    "url": "/static/js/main.431f7174.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  },
+  {
+    "revision": "ab32778d23109a7b5f778e4eca649677",
+    "url": "/static/media/landingimage.ab32778d.jpg"
+  }
+]);
